8768cbfd5a6efdc5e921ea1dcc5ad49e8c64e50b
ShinyRyo <58499405+ShinyRyo@users.noreply.github.com>
2023-12-31 07:09:19 +0000

    complete

--- src/Blockchain.ts
+++ src/Blockchain.ts
@@ -1,4 +1,5 @@
 // Blockchain.ts
+import * as fs from "fs";
 import Block from './Block';
 
 class Blockchain {
@@ -20,6 +21,23 @@ class Blockchain {
         newBlock.previousHash = this.getLatestBlock().hash;
         newBlock.hash = newBlock.calculateHash();
         this.chain.push(newBlock);
+    }
+    // ブロックチェーンの状態をファイルに保存
+    saveToFile() {
+        fs.writeFileSync('blockchain.json', JSON.stringify(this, null, 2));
+    }
+    // ファイルからブロックチェーンを読み込む（静的メソッド）
+    static loadFromFile(): Blockchain {
+        if (fs.existsSync('blockchain.json')) {
+            const data = fs.readFileSync('blockchain.json', 'utf8');
+            const parsedData = JSON.parse(data);
+            const blockchain = new Blockchain();
+            blockchain.chain = parsedData.chain.map((block: any) =>
+                new Block(block.index, block.timestamp, block.data, block.previousHash)
+            );
+            return blockchain;
+        }
+        return new Blockchain();
     }
 }
 
--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,15 @@
 import Blockchain from './Blockchain';
 import Block from './Block';
 
-const myBlockchain = new Blockchain();
+// ファイルからブロックチェーンを読み込む、もしくは新しいインスタンスを作成
+let myBlockchain = Blockchain.loadFromFile();
 
 console.log("Creating some blocks...");
 myBlockchain.addBlock(new Block(1, new Date().toISOString(), { amount: 50 }));
 myBlockchain.addBlock(new Block(2, new Date().toISOString(), { amount: 30 }));
+
+// 更新されたブロックチェーンを保存
+myBlockchain.saveToFile();
 
 console.log("Blockchain:");
 console.log(JSON.stringify(myBlockchain, null, 2));
